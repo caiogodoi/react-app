4f1f5da3f85d5a94b4da6d62194fec119ac18f77
Caio Godoi <caio.godoi@gmail.com>
2018-02-14 13:22:11 -0200

    Add files via upload

---
+++ src/components/Fish.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { formatPrice } from "../helpers";
+
+class Fish extends React.Component {
+  render() {
+    const { details, index } = this.props;
+    const isAvaible = details.status === "available";
+    const buttonText = isAvaible ? "Add To Order" : "Sold Out";
+    return (
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish.name">
+          {details.name}
+          <span className="price">{formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button
+          onClick={() => this.props.addToOrder(index)}
+          disabled={!isAvaible}
+        >
+          {buttonText}
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Fish;
